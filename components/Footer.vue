5bd081e16af041481444bd7c0b53368a0401fd1e
James C. Tucker <jamesctucker@protonmail.com>
2020-08-06 13:11:05 -0500

    added Header and Footer components

---
+++ components/Footer.vue
@@ -0,0 +1,18 @@
+<template>
+  <footer class="p-8">
+    <p class="text-center">copyright 2020</p>
+  </footer>
+</template>
+
+<script>
+export default {
+  name: 'Footer',
+}
+</script>
+<style lang="scss">
+footer {
+  position: absolute;
+  bottom: 0;
+  width: 100%;
+}
+</style>
---
+++ components/Header.vue
@@ -0,0 +1,49 @@
+<template>
+  <header>
+    <nav class="nav p-8 flex justify-between">
+      <div class="brand mr-8">
+        <n-link to="/">
+          <h1>JT</h1>
+        </n-link>
+      </div>
+      <div class="nav-links flex">
+        <div class="nav-link mr-4"><n-link to="/about">About</n-link></div>
+        <div class="nav-link mr-4">
+          <n-link to="/learning">Learning</n-link>
+        </div>
+        <div class="nav-link mr-4"><n-link to="/blog">Blog</n-link></div>
+        <div class="dark-mode-toggle">
+          <button @click="toggleDarkTheme">
+            <moon-icon
+              v-if="!isDarkMode"
+              size="25"
+              class="custom-class"
+            ></moon-icon>
+            <sun-icon v-else size="25" class="custom-class"></sun-icon>
+          </button>
+        </div>
+      </div>
+    </nav>
+  </header>
+</template>
+
+<script>
+import { SunIcon, MoonIcon } from 'vue-feather-icons'
+
+export default {
+  name: 'Header',
+  components: { MoonIcon, SunIcon },
+  data() {
+    return {
+      isDarkMode: false,
+    }
+  },
+  methods: {
+    toggleDarkTheme() {
+      this.isDarkMode = !this.isDarkMode
+      console.log('dark')
+      this.$emit('changed')
+    },
+  },
+}
+</script>
